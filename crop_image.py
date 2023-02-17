06a27bc226dfd7f23edf10f24f3e51f99843f0b0
iabhiroop <100859103+iabhiroop@users.noreply.github.com>
2023-02-17 20:36:57 +0530

    Add files via upload

---
+++ crop_image.py
@@ -0,0 +1,43 @@
+import cv2
+
+refPt = []
+cropping = False
+
+def click_and_crop(event, x, y):
+    global refPt, cropping
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+        cropping = True
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        refPt.append((x, y))
+        cropping = False
+
+        cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+        cv2.imshow("image", image)
+
+image = cv2.imread("image.jpg")
+
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
+
+while True:
+    cv2.imshow("image", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("r"):
+        image = clone.copy()
+
+    elif key == ord("c"):
+        if len(refPt) == 2:
+            roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+            cv2.imshow("ROI", roi)
+            cv2.waitKey(0)
+
+
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
